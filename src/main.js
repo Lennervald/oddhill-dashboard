cb4d2b4ff2e0a45d8313fc252223adf5dfa58090
Martin Lennervald <martin.lennervald@gmail.com>
2017-05-12 14:29:41 +0200

    added support for bootstrap-vie and scss in sass format

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Add booostrap-vue-functionality
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+// Import global styles
+import './scss/main.scss';
+
 new Vue({
   el: '#app',
   render: h => h(App)
