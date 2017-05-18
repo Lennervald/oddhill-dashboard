2bc9684897de244f5708d549fd2bd7cbc9b1cee7
Martin Lennervald <martin.lennervald@gmail.com>
2017-05-18 14:21:54 +0200

    Added calc for background image and colors

--- src/components/TransitionImage.vue
+++ src/components/TransitionImage.vue
@@ -1,32 +1,45 @@
 <template>
   <div>
-    <div class="image-container is-three-quarter">
-      <img :src="randomImage()">
+    <div class="image-container" v-bind:style="backgroundImage">
+      <div class="color-filter" v-bind:style="backgroundColor"></div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-    data: function() {
+  data: function() {
+    return {
+      image: [
+        'src/assets/images/transitions/image-one.jpg',
+        'src/assets/images/transitions/image-two.jpg',
+        'src/assets/images/transitions/image-three.jpg'
+      ],
+      color: [
+        '#2D164E',
+        '#20AAC6',
+        '#F9418C'
+      ]
+    }
+  },
+  methods: {
+  
+  },
+  computed: {
+    backgroundImage: function() {
+      // get an integer between 0 and 2
+      var num = Math.floor(Math.random() * 3);
       return {
-        image: [
-          'src/assets/images/transitions/image-one.jpg',
-          'src/assets/images/transitions/image-two.jpg'
-        ]
+        background: 'url('+this.image[num]+') no-repeat center center fixed',
       }
     },
-    methods: {
-    randomImage: function(){
-      // get an integer between 0 and 1
-      var num = Math.floor(Math.random() * 2);
-      console.log(num);
-      return this.image[num];
+    backgroundColor: function(){
+      // get an integer between 0 and 2
+      var num = Math.floor(Math.random() * 3);
+      return {
+        backgroundColor: this.color[num]
+      }
     }
   }
 }
 </script>
-
-<style lang="scss" scoped>
-  @import '../assets/scss/transition-image.scss';
-</style>
