1f8da7b0efbf82347cb90dbd719bdd0f455bdf49
OlaJFrick <info@culturehero.org>
2017-05-23 15:22:14 +0200

    Cleaned up unnessesary code in Charts

--- src/components/charts/DebitChart.vue
+++ src/components/charts/DebitChart.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
-    <!-- <h3>{{projectname}}</h3> -->
     <doughnut-chart :chart-data="datacollection" :options="opts"
       :labels="{}">{{fillData()}}
     </doughnut-chart>
   </div>
 </template>
+
 <script>
   import DoughnutChart from './DoughnutChart.js';
+
   export default{
     data () {
       return {
@@ -35,14 +36,6 @@
             },
             legend: {
                 display: false,
-                // unnessesary for this view
-                // position: 'bottom',
-                // labels: {
-                //     boxWidth: 14,
-                //     padding: 20,
-                //     fontSize: 16,
-                //     fontColor: "#000"
-                // }
             }
         },
         this.datacollection = {
@@ -51,15 +44,9 @@
                 data: [this.client, this.inhouse],
                 backgroundColor: ["#F9418C", "#722FBC"],
                 borderWidth: 0,
-                // nice options
-                // hoverBackgroundColor: "#e56298",
-                // borderColor: "none",
-                // defaultFontSize: 40,
-                // boxWidth: 10
-
             }],
-        }
-      },
+          }
+        },
+      }
     }
-  }
 </script>
