11946910433a7fec6ca33dcc929338cec0afd3ae
Martin Lennervald <martin.lennervald@gmail.com>
2017-05-23 10:27:41 +0200

    Ugly fix for getting dashboard menu to change colors after a click. This will be discarded later and replaced with a better solution

--- src/components/DashboardMenu.vue
+++ src/components/DashboardMenu.vue
@@ -2,38 +2,36 @@
 	<div>
 		<div class="menu-wrapper is-parent">
       <div class="menu-box is-child">
-      <button @click="setComponent('TransitionImage')" class="tmp-btn">bakgrundsbilder</button>
-        <article @click="setComponent('Debit')" class="message">
-          <div class="nav-menu-body">
-            <p class="app-title">Debiteringsgrad</p>
-            <p class="app-number">74<span class="number-percent">%</span></p>
-            <p class="sub-title">Mars 2017</p>
-          </div>
-        </article>
-  			<div class="linebreaker"></div> 
-        <article @click="setComponent('TestPage')" class="message">
-          <div class="nav-menu-body">
-            <p class="app-title">Aktuella Projekt</p>
-            <p class="app-number">8.<span class="small-print"></span></p>
-            <p class="sub-title">Senaste månaden</p>
-          </div>
-        </article>
-  			<div class="linebreaker"></div> 
-        <article @click="setComponent('TransitionImage')" class="message">
-          <div class="nav-menu-body">
-            <p class="app-title">Nästa Event</p>
-            <p class="app-number">20<span class="small-print">April</span></p>
-            <p class="sub-title">maj 2017</p>
-          </div>
-        </article>
-  			<div class="linebreaker"></div>
-        <article @click="setComponent('BlogPost')" class="message">
-          <div class="nav-menu-body">
-            <p class="app-title">Senaste Blogginlägg</p>
-            <p class="app-number">22<span class="small-print">Juli</span></p>
-            <p class="sub-title">April 2017</p>
-          </div>
-        </article>
+        <!-- tmp btn -->
+        <button @click="setComponent('TransitionImage')" class="tmp-btn">bakgrundsbilder</button>
+
+        <div class="nav-menu-body" @click="setComponent('Debit')" :style="debit">
+          <p class="app-title">Debiteringsgrad</p>
+          <p class="app-number">74<span class="number-percent">%</span></p>
+          <p class="sub-title">Mars 2017</p>
+        </div>
+        <div class="linebreaker"></div> 
+        
+        <div class="nav-menu-body" @click="setComponent('TestPage')" :style="testpage">
+          <p class="app-title">Aktuella Projekt</p>
+          <p class="app-number">8.<span class="small-print"></span></p>
+          <p class="sub-title">Senaste månaden</p>
+        </div>
+        <div class="linebreaker"></div> 
+        
+        <div class="nav-menu-body" @click="setComponent('TransitionImage')" :style="transitionimage">
+          <p class="app-title">Nästa Event</p>
+          <p class="app-number">20<span class="small-print">April</span></p>
+          <p class="sub-title">maj 2017</p>
+        </div>
+        <div class="linebreaker"></div>
+        
+        <div class="nav-menu-body" @click="setComponent('BlogPost')" :style="blogpost">
+          <p class="app-title">Senaste Blogginlägg</p>
+          <p class="app-number">22<span class="small-print">Juli</span></p>
+          <p class="sub-title">April 2017</p>
+        </div>
+
       </div>
 		</div>
 	</div>
@@ -45,12 +43,59 @@ import { eventBus } from '../main.js';
 export default {
   data: function() {
     return {
-
+      debit: {
+        color: '#F9418C'
+      },
+      testpage: {
+        color: '#242323'
+      },
+      transitionimage: {
+        color: '#242323'
+      },
+      blogpost: {
+        color: '#242323'
+      }
     }
   },
   methods: {
     setComponent: function(cmp){
       eventBus.$emit('setNewComponent', cmp);
+      // change text color on clicked cpm
+      switch (cmp) {
+        case 'Debit':
+        this.debit.color = '#F9418C';
+        this.testpage.color = '#242323';
+        this.transitionimage.color = '#242323';
+        this.blogpost.color = '#242323';
+        break;
+
+        case 'TestPage':
+        this.debit.color = '#242323';
+        this.testpage.color = '#F9418C';
+        this.transitionimage.color = '#242323';
+        this.blogpost.color = '#242323';
+        break;
+
+        case 'TransitionImage':
+        this.debit.color = '#242323';
+        this.testpage.color = '#242323';
+        this.transitionimage.color = '#F9418C';
+        this.blogpost.color = '#242323';
+        break;
+
+        case 'BlogPost':
+        this.debit.color = '#242323';
+        this.testpage.color = '#242323';
+        this.transitionimage.color = '#242323';
+        this.blogpost.color = '#F9418C';
+        break;
+
+        default:
+        this.debit.color = '#F9418C';
+        this.testpage.color = '#242323';
+        this.transitionimage.color = '#242323';
+        this.blogpost.color = '#242323';
+      }
     }
   }
 }
