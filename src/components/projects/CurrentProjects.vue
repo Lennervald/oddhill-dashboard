c878ae2381e9f8ea3f7af9f0c947b2ea5cab3f8d
OlaJFrick <info@culturehero.org>
2017-06-01 18:14:14 +0200

    CurrentProject cleaned up and improved.

--- src/components/projects/CurrentProjects.vue
+++ src/components/projects/CurrentProjects.vue
@@ -9,21 +9,20 @@
       </div>
     </div>
     <transition name="slide" mode="out-in">
-      <component :is="selectedComponent"
+      <component
+        :is="selectedComponent"
         :data="data"
-        :projnumber="number">
-      </component>
-    </transition>
+        :selectedNumber="selectedIndex">
+    </component>
+  </transition>
     <div class="carousel">
       <div class="block">
-        <li v-for="(project, index) in projects">
-          <div class="project-circle"
-          @click="selectSwitch(index)"
-          :class="{'active': activeItemId === index}">
-        </div>
+        <li v-for="(project, index) in projectCount">
+          <div class="project-circle" @click="selectSwitch(index)" :class="{'active': activeItemId === index}">
+          </div>
         </li>
+      </div>
     </div>
-  </div>
     <div class="hero-foot is-vcentered">
       <div class="columns">
         <div class="column has-text-left is-hidden-desktop">
@@ -39,138 +38,50 @@
 import Project0 from './Project0.vue';
 import Project1 from './Project1.vue';
 import Project2 from './Project2.vue';
-import Project3 from './Project2.vue';
-import Project4 from './Project2.vue';
+import Project3 from './Project3.vue';
+import Project4 from './Project4.vue';
+import Project5 from './Project5.vue';
+import Project6 from './Project6.vue';
+import Project7 from './Project7.vue';
+import Project8 from './Project8.vue';
+import Project9 from './Project9.vue';
+import Project10 from './Project10.vue';
 
 export default {
-  props: ['data','title', 'content', 'active', 'index'],
+  props: ['data'],
   components: {
     appProject0: Project0,
     appProject1: Project1,
     appProject2: Project2,
     appProject3: Project3,
-    appProject4: Project4
+    appProject4: Project4,
+    appProject5: Project5,
+    appProject6: Project6,
+    appProject7: Project7,
+    appProject8: Project8,
+    appProject9: Project9,
+    appProject10: Project10,
   },
   data: function() {
     return {
-      selectedComponent: 'appProject0',
-      activeItemId: '',
       pagetitle: "Aktuella Projekt",
-      projectObj: ["Commits", "Timmar"],
-      valueCommits: "178",
-      valueHours: '',
-      team: '',
-      profileImages: '',
-      projects: '',
-      number: '0',
-      selectedProject: '1',
-      projectinfo: {
-        name: '',
-        hours: '',
-        commits: '',
-      },
-      items: [
-      	{title: 'Title 1', content: 'Content 1'},
-        {title: 'Title 2', content: 'Content 2'}
-      ],
+      selectedComponent: 'appProject0',
+      selectedIndex: '0',
+      projectCount: '',
+      activeItemId: '',
       activeLiIndex: null
     }
   },
-  created: function() {
-    this.selectProj();
-    this.setupProject();
-  },
   mounted() {
+    this.projectCount = this.data.projects.length;
     this.activeItemId = 0;
-    // console.log(this.data.projects[0].teamMembers[0].name);
-    // console.log(this.data.projects.length)
   },
   methods: {
-    toggleActive: function() {
-       this.activeItemId = itemIndex;
-    },
     selectSwitch: function(indexNumber) {
       this.selectedComponent = 'appProject' + indexNumber;
-      this.number = indexNumber;
+      this.selectedIndex = indexNumber;
       this.activeItemId = indexNumber;
-    },
-    selectProj: function() {
-      this.projects = this.data.projects;
-    },
-    setupProject: function() {
-      let activeProject = this.data.projects[this.selectedProject];
-      this.projectinfo.name = activeProject.name;
-      let projMembers = activeProject.teamMembers;
-
-      let membArr = [];
-      let hourArr = [];
-      let commitArr = [];
-      let hourSum = 0;
-      let commitSum = 0;
-
-      projMembers.map(function(teamie) {
-        let memName = teamie.name.toLowerCase();
-        let memHours = parseInt(teamie.hours);
-        let memCommits = parseInt(teamie.commits);
-        membArr.push(memName);
-        hourArr.push(memHours);
-        commitArr.push(memCommits);
-      });
-
-      for (let i = 0; i < hourArr.length; i++) {
-        hourSum += hourArr[i];
-      }
-
-      for (let i = 0; i < commitArr.length; i++) {
-        commitSum += commitArr[i];
-      }
-
-      this.projectinfo.hours = hourSum;
-      this.projectinfo.commits = commitSum;
-      this.projectinfo.team = validateStaff(membArr);
-
-      function validateStaff(projMembers) {
-        let oddHillStaff = [
-          "anton",
-          "bjorn",
-          "calle",
-          "christoffer",
-          "daniel",
-          "emil",
-          "erik",
-          "hannele",
-          "jennifer",
-          "jens_g",
-          "jens_l",
-          "johan",
-          "mads",
-          "magnus",
-          "marcus",
-          "marri",
-          "mattias",
-          "michaela",
-          "mohamed",
-          "mohammed",
-          "nils",
-          "ola",
-          "olof",
-          "pia"
-        ]
-
-        let valid = [];
-        let invalid = [];
-
-        for (var i = 0; i < projMembers.length; i++) {
-          if (oddHillStaff.indexOf(projMembers[i]) !== -1) {
-            valid.push(projMembers[i]);
-          } else {
-            invalid.push(projMembers[i]);
-            console.log("Warning. Following Member(s): " + invalid + "  is/are not known to our database, and will therefor not be displayed. Please update with a full name/image asap.");
-          }
-        }
-        return valid;
-      }
-    },
+    }
   }
 }
 </script>
--- src/components/projects/Project10.vue
+++ src/components/projects/Project10.vue
@@ -1,135 +1,116 @@
 <template>
-    <div class="hero-body project-body is-vcentered">
-      <div class="columns ">
-        <div class="column is-offset-1 is-10">
-          <div class="columns">
-            <div class="column is-6">
-            </div>
-            <div class="column">
-              <section class="title-wrapper has-text-right">
-                <p class="subtitle project-values under-lined">{{valueCommits}} {{projectObj[0]}} | {{valueHours}} {{projectObj[1]}}</p>
-                <p class="title project-title">{{projectname}}</p>
-                <h1>{{test}}</h1>
-              </section>
-            </div>
-          </div>
-
-
-
-          <!-- HERE STARTS DESKTOP -->
-          <!-- <div class="tile is-hidden-mobile"> -->
-          <div class="tile">
-            <div class="tile is-ancestor">
-              <div class="tile is-parent">
-                <div class="tile is-vertical">
-                  <article class="tile is-child notification img-box row-height1">
-                    <!-- TEAM MEMBER 4 -->
-                    <div class="pic-filler4" v-bind:style="bgImg4">
-                      <div class="color-filter"></div>
+<div class="hero-body project-body is-vcentered">
+  <div class="columns ">
+    <div class="column is-offset-1 is-10">
+      <div class="columns">
+        <div class="column is-6">
+        </div>
+        <div class="column">
+          <section class="title-wrapper has-text-right">
+            <p class="subtitle project-values under-lined">{{projectCommits}} {{projectObj[0]}} | {{projectHours}} {{projectObj[1]}}</p>
+            <p class="title project-title">{{projectName}}</p>
+          </section>
+        </div>
+      </div>
+      <div class="tile">
+        <div class="tile is-ancestor">
+          <div class="tile is-parent">
+            <div class="tile is-vertical">
+              <article class="tile is-child notification img-box row-height1">
+                <div class="pic-filler4" v-bind:style="bgImg4">
+                  <div class="color-filter"></div>
+                </div>
+              </article>
+              <div class="tile is-ancestor">
+                <div class="tile is-parent is-4">
+                  <article class="tile is-child notification img-box">
+                    <div class="color-filter"></div>
+                    <div class="pic-filler3" v-bind:style="bgImg3">
                     </div>
                   </article>
-                  <div class="tile is-ancestor">
-                    <div class="tile is-parent is-4">
-                      <!-- TEAM MEMBER 3 -->
-                      <article class="tile is-child notification img-box">
-                        <div class="color-filter"></div>
-                        <div class="pic-filler3" v-bind:style="bgImg3">
+                </div>
+                <div class="tile is-vertical">
+                  <div class="tile is-vertical">
+                    <div class="tile is-parent">
+                      <article class="tile is-child notification img-box row-height2">
+                        <div class="pic-filler2" v-bind:style="bgImg2">
+                          <div class="color-filter"></div>
                         </div>
                       </article>
                     </div>
-                    <div class="tile is-vertical">
-                      <div class="tile is-vertical">
-                        <div class="tile is-parent">
-                          <article class="tile is-child notification img-box row-height2">
-                            <!-- TEAM MEMBER 2 -->
-                            <div class="pic-filler2" v-bind:style="bgImg2">
-                              <div class="color-filter"></div>
-                            </div>
-                          </article>
-                        </div>
-                        <div class="tile">
-                          <div class="tile is-parent">
-                            <article class="tile is-child notification img-box">
-                              <!-- TEAM MEMBER 8 -->
-                              <div class="pic-filler8" v-bind:style="bgImg8">
-                                <div class="color-filter"></div>
-                              </div>
-                            </article>
+                    <div class="tile">
+                      <div class="tile is-parent">
+                        <article class="tile is-child notification img-box">
+                          <div class="pic-filler8" v-bind:style="bgImg8">
+                            <div class="color-filter"></div>
                           </div>
-                          <div class="tile is-parent">
-                            <article class="tile is-child notification img-box row-height3">
-                              <!-- TEAM MEMBER 5 -->
-                              <div class="pic-filler5" v-bind:style="bgImg5">
-                                <div class="color-filter col-filter2"></div>
-                              </div>
-                            </article>
+                        </article>
+                      </div>
+                      <div class="tile is-parent">
+                        <article class="tile is-child notification img-box row-height3">
+                          <div class="pic-filler5" v-bind:style="bgImg5">
+                            <div class="color-filter col-filter2"></div>
                           </div>
-                        </div>
+                        </article>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-              <div class="tile">
-                <div class="tile is-parent is-vertical">
-                  <article class="tile is-child notification img-box">
-                    <!-- TEAMMEMEBER 7  + ILLUSTRATIVE IMAGE 2 -->
-                    <div class="illustrative-filler2 pic-filler7">
-                      <div class="color-filter"></div>
-                    </div>
-                  </article>
-                  <article class="tile is-child notification img-box">
-                    <!-- TEAM MEMBER 1 -->
-                    <div class="pic-filler1 small-horizonal" v-bind:style="bgImg1">
-                      <div class="color-filter"></div>
-                    </div>
-                  </article>
-                </div>
-                <div class="tile is-parent is-vertical">
-                  <article class="tile is-child col-filter1">
-                  </article>
-                  <article class="tile is-child img-box">
-                    <!-- TEAM MEMBER 9 -->
-                    <div class="pic-filler9" v-bind:style="bgImg9">
-                      <div class="color-filter"></div>
-                    </div>
-                  </article>
-                  <article class="tile is-child notification img-box row-height4">
-                    <!-- TEAM MEMBER 6 -->
-                    <div class="pic-filler6" v-bind:style="bgImg6">
-                      <div class="color-filter col-filter1"></div>
-                    </div>
-                  </article>
-                </div>
-
-              </div>
             </div>
           </div>
-          <!-- HERE ENDS DESKTOP -->
+          <div class="tile">
+            <div class="tile is-parent is-vertical">
+              <article class="tile is-child notification img-box">
+                <div class="illustrative-filler2 pic-filler7">
+                  <div class="color-filter"></div>
+                </div>
+              </article>
+              <article class="tile is-child notification img-box">
+                <div class="pic-filler1 small-horizonal" v-bind:style="bgImg1">
+                  <div class="color-filter"></div>
+                </div>
+              </article>
+            </div>
+            <div class="tile is-parent is-vertical">
+              <article class="tile is-child col-filter1">
+              </article>
+              <article class="tile is-child img-box">
+                <div class="pic-filler9" v-bind:style="bgImg9">
+                  <div class="color-filter"></div>
+                </div>
+              </article>
+              <article class="tile is-child notification img-box row-height4">
+                <div class="pic-filler6" v-bind:style="bgImg6">
+                  <div class="color-filter col-filter1"></div>
+                </div>
+              </article>
+            </div>
 
+          </div>
         </div>
       </div>
     </div>
+  </div>
+</div>
 </template>
 
 <script>
-
 export default {
-  props: ['data', 'projnumber'],
+  props: ['data', 'selectedNumber'],
   data: function() {
     return {
-      test: this.projnumber,
-      currentProj: '',
-      projectname: '',
+      number: this.selectedNumber,
       projectObj: ['Commits', 'Timmar'],
-      valueCommits: '',
-      valueHours: '',
+      projectName: this.data.projects[this.selectedNumber].name,
+      projectCommits: '',
+      projectHours: '',
       team: '',
       profileImages: '',
     }
   },
   created: function() {
-    this.setupProject();
+    this.calculate();
     this.generateImages();
   },
   mounted() {
@@ -137,22 +118,22 @@ export default {
     // console.log(this.data.projects.length)
   },
   methods: {
-    setupProject: function() {
-      this.currentProj = this.data.projects[this.test];
-      let selectedProj = this.currentProj;
-      let projMembers = selectedProj.teamMembers;
-      this.projectname = selectedProj.name;
+    calculate: function() {
 
-      let membArr = [];
-      let hourArr = [];
-      let commitArr = [];
-      let hourSum = 0;
-      let commitSum = 0;
+      let project = this.data.projects[this.number],
+        fetchMembers = project.teamMembers;
+      console.log(fetchMembers);
 
-      projMembers.map(function(teamie) {
-        let memName = teamie.name.toLowerCase();
-        let memHours = parseInt(teamie.hours);
-        let memCommits = parseInt(teamie.commits);
+      let membArr = [],
+        hourArr = [],
+        commitArr = [],
+        hourSum = 0,
+        commitSum = 0;
+
+      fetchMembers.map(function(staff) {
+        let memName = staff.name.toLowerCase();
+        let memHours = parseInt(staff.hours);
+        let memCommits = parseInt(staff.commits);
         membArr.push(memName);
         hourArr.push(memHours);
         commitArr.push(memCommits);
@@ -166,9 +147,13 @@ export default {
         commitSum += commitArr[i];
       }
 
-      this.valueHours = hourSum;
-      this.valueCommits = commitSum;
-      this.team = validateStaff(membArr);
+      this.projectHours = hourSum;
+      this.projectCommits = commitSum;
+      this.team = membArr;
+
+    },
+    generateImages: function() {
+      let team = validateStaff(this.team);
 
       function validateStaff(projMembers) {
         let oddHillStaff = [
@@ -211,9 +196,7 @@ export default {
         }
         return valid;
       }
-    },
-    generateImages: function() {
-      let team = this.team;
+
       let imgPaths = [];
 
       for (let i = 0; i < team.length; i++) {
